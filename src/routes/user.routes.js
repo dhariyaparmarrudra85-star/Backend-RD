0daa1b69216bb3fa476af4e2c26a3e014046cc62
Rudra <dhariyaparmarrudra85@gmail.com>
2026-02-21 16:58:36 +0530

    Refactor user management: update logout functionality to unset refresh token and rename user model; enhance user routes with additional account management features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser} from "../controllers/user.controllers.js";
+import {changeCurrentPassword, getCurrentUser, getUserchannelprofile, getWatchHistory, loginUser, logoutUser, registerUser, updateAccountDetails, updateAvatar, updateCoverImage} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controllers.js";
@@ -25,4 +25,20 @@ router.route("/login").post(loginUser)
 // secured route
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT , upload.single("avatar") , updateAvatar)
+
+router.route("/coverImage").patch(verifyJWT , upload.single("coverImage") , updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT , getUserchannelprofile)
+
+router.route("/history").get(verifyJWT , getWatchHistory)
+
+
 export default router
